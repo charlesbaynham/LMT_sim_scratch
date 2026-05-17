4f27e97458c5566d4cff4f15e38be604f30a7ca9
Charles Baynham <c.baynham@imperial.ac.uk>
2026-05-17 13:24:22 +0100

    Move

--- tests/test_states_vector.py
+++ tests/test_states_vector.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import numpy as np
 import pytest
 
-sys.path.insert(0, str(Path(__file__).resolve().parent))
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
 
 import lmt_sim.lmt_simulation as sim
 
